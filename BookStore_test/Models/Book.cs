4ee33f320d6b6e213d8171666c33669d2b6611b9
yar.gord8ASdYa9KEnny <yar.gorda asczcvxasd89@gmail.com>
2019-01-08 18:42:51 +0200

    Project Start

---
+++ BookStore_test/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore_test.Models
+{
+    public class Book
+    {
+        //ID
+        public int Id { get; set; }
+        //Book`s name
+        public string Name { get; set; }
+        //автор книги
+        public string Author { get; set; }
+        //Цена
+        public int Price { get; set; }
+    }
+}
---
+++ BookStore_test/Models/Purchase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore_test.Models
+{
+    public class Purchase
+    {
+        //Purchase ID
+        public int PurchaseID { get; set; }
+        //Name and second name of buyer
+        public string Person { get; set; }
+        //Adress of buyer
+        public string Adress { get; set; }
+        //ID of book
+        public int BookID { get; set; }
+        //Purchase Date
+        public DateTime Date { get; set; }
+    }
+}
